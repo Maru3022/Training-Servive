1114aead0c0a7352f3cd3e208ab3195c6d5a9d37
Maru <pdijsj2992@gmail.com>
2026-03-31 17:35:24 +0300

    Revert "Fix CI/CD: configure H2 for tests, fix CORS configuration"
    This reverts commit a1721d292b0161321e0bd1b06bdd8d210b38e8fe.

--- src/test/java/com/example/training_service/TrainingServiceApplicationTests.java
+++ src/test/java/com/example/training_service/TrainingServiceApplicationTests.java
@@ -1,11 +1,7 @@
 package com.example.training_service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
 class TrainingServiceApplicationTests {
 
     @Test
